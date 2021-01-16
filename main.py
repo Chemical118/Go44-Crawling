ec8d837f2ae57ac0fe36348333fe6dbf48ac0bba
Chemical118 <59816794+Chemical118@users.noreply.github.com>
2021-01-16 15:58:59 +0900

    버그 수정

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ last_name = "가,간,갈,감,강,개,경,계,고,곡,공,곽,교,구,국,군,궁
             "박,반,방,배,백,변,범,복,봉,부,비,빈,빙,사,삼,상,서,석,선,설,섭,성,소,손,송,수,순,승,시,신,심,십,아,안,애,야,양,어,엄,여,연,염,엽,영,예,오,옥,온,옹,왕,요,용," \
             "우,운,원,위,유,육,윤,은,음,이,인,임,자,장,저,전,점,정,제,조,종,좌,주,준,즙,증,지,진,차,창,채,천,초,최,추,춘,탁,탄,태,판,팽,편,평,포,표,풍,피,필,하,학,한,함," \
             "해,허,현,호,홍,화,환,황,후,흥"
-cnt = 0
+
 url = "https://go.sasa.hs.kr/autocomplete/get_hak2?term="
-all_cnt = 0
 conti_check = False
+student_list = [[] for _ in range(18)]
+temp_student_list = [[] for _ in range(4)]
+
 with open("id.txt", "r") as f:
     _id, _pw = f.readlines()
+
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 
-driver = webdriver.Chrome("chromedriver", options=options)
+driver = webdriver.Chrome("chromedriver")
 
 driver.implicitly_wait(3)
 driver.get("https://go.sasa.hs.kr/auth")
@@ -26,8 +29,6 @@ driver.find_element_by_name('id').send_keys(_id)
 driver.find_element_by_name('passwd').send_keys(_pw)
 driver.find_element_by_xpath("/html/body/div/div[2]/form/div[3]/div[3]/input").click()
 
-student_list = [[] for _ in range(18)]
-temp_student_list = [[] for _ in range(4)]
 for last in last_name.split(','):
     driver.get(url + parse.quote(last))
     namelist = BeautifulSoup(driver.page_source, 'html.parser').text.split("\n")[-1][1:-1]  # 에러 회피
@@ -41,7 +42,6 @@ for last in last_name.split(','):
                 _name = _name.encode('utf-8')
                 _name = _name.decode('unicode_escape')
                 class_chr = name.split(' ')[0]
-                t = class_chr
                 if class_chr == "" or class_chr[2] == "0":  # 학번이 변경중인 경우
                     if class_chr == "":
                         temp_student_list[3].append(_name)
@@ -51,15 +51,14 @@ for last in last_name.split(','):
                 else:  # 학번이 정해진 경우
                     _grade = int(class_chr[0])
                     _class = int(class_chr[2])
-                    student_list[class_chr[0] * 6 + class_chr[2] - 7].append(_name)
+                    student_list[_grade * 6 + _class - 7].append(_name)
                     temp_student_list[_grade - 1].append(_name)
-                cnt += 1
 driver.quit()
-print("전체 학생은 %d명입니다!" % cnt)
+len_tuple = tuple(len(i) for i in temp_student_list)
+print("전체 학생은 %d명입니다!" % sum(len_tuple))
 if conti_check:
     print("반배정이 결정되지 않았습니다")
-    print("1학년 : %d명\n2학년 : %d명\n3학년 : %d명\n졸업생 : %d명" % (
-        len(temp_student_list[0]), len(temp_student_list[1]), len(temp_student_list[2]), len(temp_student_list[3])))
+    print("1학년 : %d명\n2학년 : %d명\n3학년 : %d명\n졸업생 : %d명" % len_tuple)
     for i in range(4):
         print("-----------------------")
         if i == 3:
@@ -68,9 +67,9 @@ if conti_check:
             print("%d학년" % (i + 1))
         for j in temp_student_list[i]: print(j)
 else:
+    len_tuple = len_tuple[:-1]
+    print("1학년 : %d명\n2학년 : %d명\n3학년 : %d명" % len_tuple)
     for i in range(18):
-        print("1학년 : %d명\n2학년 : %d명\n3학년 : %d명" % (
-            len(temp_student_list[0]), len(temp_student_list[1]), len(temp_student_list[2])))
         print("%d-%d" % (i / 6 + 1, i % 6 + 1))
         cnt = 1
         for st in student_list[i]:
